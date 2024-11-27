9241c2d2de4a8df02ed4947d11e85967768a7f1f
wian0105 <wian.sanjaya0801@gmail.com>
2024-11-27 16:46:47 +0700

    Add: calendar

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,25 @@ import 'flowbite';
 import "../css/satoshi.css";
 import "../css/app.css";
 
+// full calendar
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const calendarEl = document.getElementById('calendar');
+    if (calendarEl) {
+        const calendar = new Calendar(calendarEl, {
+            plugins: [dayGridPlugin, interactionPlugin],
+            initialView: 'dayGridMonth',
+            events: '/path-to-your-events-endpoint', // Add an API or array of events here
+        });
+        calendar.render();
+    }
+});
+// end fullcalendar
+
 import persist from "@alpinejs/persist";
 
 import Alpine from 'alpinejs';
