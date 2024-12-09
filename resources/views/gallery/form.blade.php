111280401ecc8deecd84db61a93a94a567cd74fe
wian0105 <wian.sanjaya0801@gmail.com>
2024-12-09 13:29:21 +0700

    Fix: Fillter (Not Yet)

--- resources/views/gallery/form.blade.php
+++ resources/views/gallery/form.blade.php
@@ -44,7 +44,6 @@
         @else
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 @foreach($galleries as $gallery)
-                    @if(request('category') === '' || $gallery->category === request('category'))
                         <div class="border border-gray-300 rounded-md p-2 text-center image-item" data-category="{{ $gallery->category }}">
                             <img src="{{ asset('storage/' . $gallery->image) }}" alt="{{ $gallery->title }}" class="w-full h-auto mb-2 rounded">
                             <form action="{{ route('gallery.destroy', $gallery->id) }}" method="POST" class="inline">
@@ -53,7 +52,6 @@
                                 <button type="submit" class="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600">Delete</button>
                             </form>
                         </div>
-                    @endif
                 @endforeach
             </div>
         @endif
